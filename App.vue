288d6805879ca932d4c8f844b5ab53c7d9777439
OLiang24 <68525467+OLiang24@users.noreply.github.com>
2020-08-21 21:42:26 +0700

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,113 @@
+<template>
+  <div id="app">
+    <img src="./assets/d11.png" alt="">
+    <div>
+      <slide />
+    </div>
+
+    <div class="container">
+      <div class="btn btn-light" >
+        <div class="row 12">
+          <div class="col-10 ">
+            <img src="./assets/a.jpg" alt width="70%" >
+            <counter @cc="sum1"></counter>
+          </div>
+          <div class="col-2 ">
+            <h3>
+              Sum
+              <ul>{{total1}}</ul>
+            </h3>
+          </div>
+        </div>
+      </div>
+      <br />
+      <br />
+      <div class="btn btn-light">
+        <div class="row 12">
+          <div class="col-10 ">
+            <img src="./assets/b.jpg" alt width="70%" >
+            <counter @cc="sum2"></counter>
+          </div>
+
+          <div class="col-2 ">
+            <h3>
+              Sum
+              <ul>{{total2}}</ul>
+            </h3>
+          </div>
+        </div>
+      </div>
+      <br />
+      <br />
+      <div class="btn btn-light">
+        <div class="row 12">
+          <div class="col-10 ">
+            <img src="./assets/c.png" alt width="70%" >
+            <br />
+            <counter @cc="sum3"></counter>
+          </div>
+          <div class="col-2 ">
+            <h3>
+              Sum
+              <ul>{{total3}}</ul>
+            </h3>
+          </div>
+        </div>
+      </div>
+      <br />
+      <br />
+    </div>
+  </div>
+</template>
+
+<script>
+import Counter from "./components/Counter.vue";
+import slide from "./components/slide.vue";
+
+export default {
+  name: "App",
+  components: {
+    Counter,
+    slide,
+  },
+
+  data: function () {
+    return {
+      total: 0,
+      total1: 0, //ผลรวมทั้งหมด --> เอาไปแสดงผล {{}}
+      total2: 0,
+      total3: 0,
+    };
+  },
+
+  methods: {
+    // 3. คำนวนผลรวมทั้งหมดที่ส่งออกมาจาก emit
+    // แล้วเก็บผลรวมไว้ที่ this.total
+    sum(CounterValue) {
+      this.total = CounterValue;
+    },
+    sum1(CounterValue) {
+      this.total1 = CounterValue;
+    },
+    sum2(CounterValue) {
+      this.total2 = CounterValue;
+    },
+    sum3(CounterValue) {
+      this.total3 = CounterValue;
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  background-image: url(./assets/p.jpg);
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #3886d4;
+  margin-top: 60px;
+}
+
+</style>
---
+++ components/slide.vue
@@ -0,0 +1,59 @@
+<template>
+        <div class="container ">
+            
+      <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img :src="img[0]"  wi/>
+          </div>
+          <div class="carousel-item">
+            <img :src="img[1]"  />
+          </div>
+          <div class="carousel-item">
+            <img :src="img[2]"  />
+          </div>
+        </div>
+        <a
+          class="carousel-control-prev"
+          href="#carouselExampleControls"
+          role="button"
+          data-slide="prev"
+        >
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a
+          class="carousel-control-next"
+          href="#carouselExampleControls"
+          role="button"
+          data-slide="next"
+        >
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+        <br>
+        <hr>
+        <br>
+      </div>
+    </div>
+
+
+
+</template>
+<script>
+export default {
+    data: function(){
+        return{
+            img:[
+                require("../assets/1.jpg"),
+                require("../assets/2.jpg"),
+                require("../assets/3.jpg"),
+            ],
+        };
+    },
+
+};
+</script>
+<style>
+
+</style>
